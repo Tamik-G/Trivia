8ec82cc29c529424f6012e17890f311cbe8eacc9
Tamik-G <timurchaershin@mail.ru>
2025-04-16 11:19:27 +0300

    MY first socket

---
+++ test/first_server.py
@@ -0,0 +1,17 @@
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# noinspection PyTypeChecker
+sock.bind(("127.0.0.1", 5566))
+clients = []
+
+print("Start Server")
+while True:
+    data, adress = sock.recvfrom(1024)
+    print(f"Message from [{adress}]: {data.decode('utf-8')}")
+    if adress not in clients:
+        clients.append(adress)
+    for client in clients:
+        if client == adress:
+            continue
+        sock.sentdo(data, client)
--- test/test_rounds_05_steps.py
+++ test/test_rounds_05_steps.py
@@ -5,4 +5,3 @@ def test_rounds_05_steps():
     assert (solution(4.1) == 4)
     assert (solution(4.8) == 5)
     assert (solution(3.6) == 3.5)
-
